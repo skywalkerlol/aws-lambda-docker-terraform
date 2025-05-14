a17808857a1ebd6f8b86f2c7f43c9a6a0cfb7942
Sourav Kundu <skundu.dev@gmail.com>
2025-05-14 09:12:28 -0500

    added lambda and kms key

--- terraform_lambda/lambda.tf
+++ terraform_lambda/lambda.tf
@@ -1,60 +1,103 @@
-# # lambda.tf
-# resource "aws_lambda_function" "container_lambda" {
-#   function_name = "${var.name}-lambda"
-#   role          = aws_iam_role.lambda_role.arn
-#   package_type  = "Image"
-#   image_uri     = "${aws_ecr_repository.this.repository_url}:latest"
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/lambda_function
+resource "aws_lambda_function" "container_lambda" {
+  function_name = "${var.name}-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  package_type  = "Image"
+  image_uri     = var.image_uri
+  memory_size   = var.memory_size
+  timeout       = var.timeout
+  reserved_concurrent_executions = var.reserved_concurrency
+  kms_key_arn   = aws_kms_key.encrypt_lambda_environments.arn
+  logging_config {
+    log_format       = "JSON"
+    log_group        = aws_cloudwatch_log_group.lambda_log.name
+    system_log_level = "INFO"
+  }
+    tracing_config {
+    mode = "Active"
+  }
+  dead_letter_config {
+    target_arn = aws_sqs_queue.lambda_dead_letter_queue.arn
+  }
+}
 
-#   memory_size = 128
-#   timeout     = 30
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.name}-lambda-role"
 
-#   environment {
-#     variables = {
-#       ENVIRONMENT = "production"
-#     }
-#   }
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-# # IAM role for Lambda
-# resource "aws_iam_role" "lambda_role" {
-#   name = "${var.name}-lambda-role"
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+locals {
+  image_uri      = var.image_uri
+  ecr_account_id = split(".", local.image_uri)[0]
+  ecr_region     = split(".", local.image_uri)[3]
+  ecr_repo_path  = split("/", local.image_uri)[1]
+  ecr_repo_name  = split(":", local.ecr_repo_path)[0]
 
-# # Basic Lambda execution policy
-# resource "aws_iam_role_policy_attachment" "lambda_basic" {
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-#   role       = aws_iam_role.lambda_role.name
-# }
+  ecr_repo_arn = "arn:aws:ecr:${local.ecr_region}:${local.ecr_account_id}:repository/${local.ecr_repo_name}"
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
+resource "aws_iam_role_policy" "lambda_ecr_access" {
+  name = "${var.name}-lambda-ecr-access"
+  role = aws_iam_role.lambda_role.id
 
-# # ECR access policy for Lambda
-# resource "aws_iam_role_policy" "lambda_ecr_access" {
-#   name = "${var.name}-lambda-ecr-access"
-#   role = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = [local.ecr_repo_arn]
+      }
+    ]
+  })
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
+resource "aws_iam_role_policy" "lambda_dlq_and_kms_access" {
+  name = "${var.name}-lambda-dlq-kms-access"
+  role = aws_iam_role.lambda_role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "ecr:GetDownloadUrlForLayer",
-#           "ecr:BatchGetImage"
-#         ]
-#         Resource = [aws_ecr_repository.this.arn]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowSendMessageToDLQ",
+        Effect = "Allow",
+        Action = [
+          "sqs:SendMessage"
+        ],
+        Resource = aws_sqs_queue.lambda_dead_letter_queue.arn
+      },
+      {
+        Sid    = "AllowDecryptKMSForDLQ",
+        Effect = "Allow",
+        Action = [
+          "kms:Decrypt",
+          "kms:GenerateDataKey"
+        ],
+        Resource = aws_kms_key.encrypt_sqs.arn
+      }
+    ]
+  })
+}
---
+++ terraform_lambda/lambda_kms.tf
@@ -0,0 +1,46 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
+resource "aws_kms_key" "encrypt_lambda_environments" {
+  enable_key_rotation     = true
+  description             = "Key to encrypt the Lambda environment variables in ${var.name}."
+  deletion_window_in_days = 7
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_alias
+resource "aws_kms_alias" "encrypt_lambda_environments" {
+  name          = "alias/${var.name}-encrypt-lambda-environment"
+  target_key_id = aws_kms_key.encrypt_lambda_environments.key_id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+data "aws_iam_policy_document" "encrypt_lambda_environments_policy" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = [local.principal_root_arn]
+    }
+  }
+  statement {
+    sid    = "Allow Lambda to use the key"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+      "kms:CreateGrant"
+    ]
+    resources = [aws_kms_key.encrypt_lambda_environments.arn]
+  }
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key_policy
+resource "aws_kms_key_policy" "encrypt_lambda_environments" {
+  key_id = aws_kms_key.encrypt_lambda_environments.id
+  policy = data.aws_iam_policy_document.encrypt_lambda_environments_policy.json
+}
