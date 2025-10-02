68ae37aa7fcc93b4be2f2e00136fb570cf88a166
ashingote <65078323+ashingote@users.noreply.github.com>
2025-10-01 21:27:42 -0400

    update tf-state bucket name

--- terraform_ecr/backend.tf
+++ terraform_ecr/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "kunduso-terraform-remote-bucket"
+    bucket       = "skywalker-tf-state-bucket"
     encrypt      = true
     key          = "tf/aws-lambda-docker-terraform/terraform_ecr/terraform.tfstate"
     region       = "us-east-2"
